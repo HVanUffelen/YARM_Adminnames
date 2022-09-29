7c6d142941a0712cfac163c359bc1606fe0649ea
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-09-29 14:33:10 +0200

    Start package

--- src/AdminNamesServiceProvider.php
+++ src/AdminNamesServiceProvider.php
@@ -9,9 +9,9 @@ class AdminNamesServiceProvider extends ServiceProvider{
     public function boot()
     {
 
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/views','adminnames');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        //$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        //$this->loadViewsFrom(__DIR__ . '/views','adminnames');
+        //$this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->mergeConfigFrom(__DIR__ . '/config/AdminNames.php','adminnames');
         $this->publishes([
             //__DIR__ . '/config/bookshelf.php' => config_path('bookshelf.php'),
