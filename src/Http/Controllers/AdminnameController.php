1a48b5ac8d01465ab9b0aeb5b72a4bef8eba21e2
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-09-29 15:57:00 +0200

    Start package

---
+++ src/Http/Controllers/AdminnameController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Yarm\Adminnames\Http\Controllers;
+
+use App\Models\Name;
+use App\Models\Group;
+use Illuminate\Http\Request;
+
+class AdminnameController extends Controller
+{
+    /**
+     * NameController constructor.
+     */
+    public function __construct()
+    {
+//        $this->middleware('can:create-ref')->only('create', 'store');
+//        $this->middleware('can:edit-ref')->only('edit');
+//        $this->middleware('can:update-ref')->only('update');
+//        $this->middleware('can:delete-ref')->only('destroy');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Name  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Name $name)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Name  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Name $name)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Name  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Name $name)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Name  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Name $name)
+    {
+        //
+    }
+
+    public function getName(Request $request)
+    {
+        return Name::find($request->id);
+    }
+
+    public function getRefNames()
+    {
+        $q = '';
+        return view('dlbt.names.refNames')->with(self::addRefNamesData($q, false));
+    }
+
+    public function getUncheckedRefNames()
+    {
+        $q = '';
+        return view('dlbt.names.refNames')->with(self::addRefNamesData($q, true));
+    }
+
+    public static function addRefNamesData($q, $checked)
+    {
+        $paginationValue = PaginationController::getPaginationItemCount();
+        $colNames = ['id','name','first_name','VIAF_id','WIKIDATA_id','checked'];
+
+        if (Auth()->user()->can('edit-only-group-ref')){
+            $groupIds = Group::listGroups4user(Auth()->user()->id, true);
+
+            $query = Name::select('name', 'first_name', 'names.id as id', 'VIAF_id', 'WIKIDATA_id', 'checked');
+            $query->leftJoin('name_ref', 'names.id', '=', 'name_ref.name_id')
+                ->leftJoin('group_ref', 'group_ref.ref_id', '=', 'name_ref.ref_id');
+            if ($checked) {
+                $query->where('names.checked','=','false');
+            }
+            $query->whereIn('group_ref.group_id', $groupIds);
+            $query->groupBy('id')
+                ->orderBy('name')
+                ->orderBy('first_name');
+
+            if ($q != '') {
+                $query->where('name', 'like', '%' . $q . '%');
+            }
+        }
+
+        $data = [
+            'names' => $query->paginate($paginationValue),
+            'colNames' => $colNames
+        ];
+
+        return $data;
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 Route::group(['namespace'=>'Yarm\Adminnames\Http\Controllers','prefix'=>'dlbt','middleware'=>['web']], function (){
 
-    Route::get('/allRefNames', 'NameController@getRefNames')
+    Route::get('/allRefNames', 'AdminnameController@getRefNames')
         ->name('get_ref_names');
-    Route::get('/allUncheckedRefNames', 'NameController@getUncheckedRefNames')
+    Route::get('/allUncheckedRefNames', 'AdminnameController@getUncheckedRefNames')
         ->name('get_unchecked_ref_names');
     Route::get('/dataCleaningManual', 'DataCleaningController@manual')
         ->name('dataCleaning_manual');
