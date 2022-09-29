52da202356e5214728d9f25672ba4f5a9477ccbd
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-09-29 14:26:07 +0200

    Start package

---
+++ src/AdminKeywordsServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Yarm\Adminnames;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminNamesServiceProvider extends ServiceProvider{
+
+    public function boot()
+    {
+
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/views','adminnames');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->mergeConfigFrom(__DIR__ . '/config/AdminNames.php','adminnames');
+        $this->publishes([
+            //__DIR__ . '/config/bookshelf.php' => config_path('bookshelf.php'),
+            //__DIR__ . '/views' => resource_path('views/vendor/adminkeywords'),
+            // Assets
+            __DIR__ . '/js' => resource_path('js/vendor'),
+        ],'adminnames');
+
+
+        //after every update
+        //run   php artisan vendor:publish --provider="Yarm\Adminnames\AdminNamesServiceProvider" --tag="adminnames" --force
+    }
+
+    public function register()
+    {
+
+    }
+
+}
---
+++ src/config/AdminNames.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+  'adminnames_present' => "True"
+
+];
