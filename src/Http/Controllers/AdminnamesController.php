05f1d94055fd0776c951dd73e5fe231a0a0fcb04
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-09-30 12:32:22 +0200

    Start package

--- src/Http/Controllers/AdminnamesController.php
+++ src/Http/Controllers/AdminnamesController.php
@@ -2,11 +2,11 @@
 
 namespace Yarm\Adminnames\Http\Controllers;
 
-use App\Http\Controllers\PaginationController;
 use App\Models\Name;
 use App\Models\Group;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\NameController;
 
 class AdminnamesController extends Controller
 {
@@ -29,44 +29,13 @@ class AdminnamesController extends Controller
     public function getRefNames()
     {
         $q = '';
-        return view('dlbt.names.refNames')->with(self::addRefNamesData($q, false));
+        return view('dlbt.names.refNames')->with(NameController::addRefNamesData($q, false));
     }
 
     public function getUncheckedRefNames()
     {
         $q = '';
-        return view('dlbt.names.refNames')->with(self::addRefNamesData($q, true));
+        return view('dlbt.names.refNames')->with(NameController::addRefNamesData($q, true));
     }
 
-    public static function addRefNamesData($q, $checked)
-    {
-        $paginationValue = PaginationController::getPaginationItemCount();
-        $colNames = ['id','name','first_name','VIAF_id','WIKIDATA_id','checked'];
-
-        if (Auth()->user()->can('edit-only-group-ref')){
-            $groupIds = Group::listGroups4user(Auth()->user()->id, true);
-
-            $query = Name::select('name', 'first_name', 'names.id as id', 'VIAF_id', 'WIKIDATA_id', 'checked');
-            $query->leftJoin('name_ref', 'names.id', '=', 'name_ref.name_id')
-                ->leftJoin('group_ref', 'group_ref.ref_id', '=', 'name_ref.ref_id');
-            if ($checked) {
-                $query->where('names.checked','=','false');
-            }
-            $query->whereIn('group_ref.group_id', $groupIds);
-            $query->groupBy('id')
-                ->orderBy('name')
-                ->orderBy('first_name');
-
-            if ($q != '') {
-                $query->where('name', 'like', '%' . $q . '%');
-            }
-        }
-
-        $data = [
-            'names' => $query->paginate($paginationValue),
-            'colNames' => $colNames
-        ];
-
-        return $data;
-    }
 }
